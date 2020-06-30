6849434a2112bf3a8bf48543b0bc9fa70761a26f
Azure Pipelines <SpeedTechSolutions@visualstudio.com>
2020-06-30 05:59:06 +0000

    Release Version 2020.06.30.393

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $nav = $('.navbar');
     $toggleCollapse = $('.toggle-collapse');
 
-    $toggleCollapse.click(function() {
+    $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            736: {
+                items: 2
+            },
+            980: {
+                items: 3
+            }
+        }
+    });
 
 });
